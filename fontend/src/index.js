e1bed64281f129700a641547137c8af0c4bc811d
miraz66 <mirajulislam0509@gmail.com>
2023-05-02 00:42:19 +0600

    all_course

--- fontend/src/index.js
+++ fontend/src/index.js
@@ -7,11 +7,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <div className="min-h-screen bg-gray-200">
-      <App />
-    </div>
-  </React.StrictMode>
+	<React.StrictMode>
+		<div className="min-h-screen bg-gray-200">
+			<App />
+		</div>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
