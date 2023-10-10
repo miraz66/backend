7ffc74d0bdef60d71600012b6eead5d1db572f40
miraz66 <mirajulislam0509@gmail.com>
2023-10-10 23:07:53 +0600

    url

--- caleb_next-js/src/app/blog/page.js
+++ caleb_next-js/src/app/blog/page.js
@@ -31,6 +31,8 @@ export default async function Blog() {
                 <div>hello</div>d<div>hello</div>d<div>hello</div>
                 <div>hello</div>d<div>hello</div>d<div>hello</div>
                 <div>hello</div>d<div>hello</div>d<div>hello</div>
+                <div>hello</div>d<div>hello</div>d<div>hello</div>
+                <div>hello</div>d<div>hello</div>d<div>hello</div>
               </div>
             ))
           : null}
